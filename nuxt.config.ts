ecd245fc32c4360339fcc7ff870487d65c43f71a
Mujahid Anuar <mujahidfanuar@gmail.com>
2023-12-13 14:10:50 +0800

    Add nuxt/image and nuxt/color-mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
+  devtools: {
+    enabled: true,
+    // Enable Timeline tracking
+    timeline: {
+      enabled: true
+    }
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@nuxt/image',
+    '@nuxtjs/color-mode'
+  ],
   typescript: {
     // Enable type-checking at build time
     typeCheck: true,
